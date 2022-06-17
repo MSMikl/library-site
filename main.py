6087c0a1e3d8205207a53bd41cd6d058a2cd6a02
MSMikl <77015283+MSMikl@users.noreply.github.com>
2022-06-17 18:46:12 +0300

    book meta data parcing added

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from pathlib import Path
 
 import requests
 
+from bs4 import BeautifulSoup
+
 Path('./books').mkdir(parents=True, exist_ok=True)
 
 def check_for_redirect(content):
@@ -9,19 +11,30 @@ def check_for_redirect(content):
         raise requests.HTTPError
     return
 
-def main():
 
+def get_book_meta(id):
+    url = f'https://tululu.org/b{id}/'
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    title = soup.find('h1').text.split('::')[0].strip()
+    author = soup.find('h1').find('a').text
+    return {'author': author, 'title': title}
+
+
+def main():
     for book in range(10):
-        url = 'https://tululu.org/txt.php?id='
-        response = requests.get(url=f'{url}{book}')
+        url = f'https://tululu.org/txt.php?id={book}'
+        response = requests.get(url=url)
         response.raise_for_status()
         try:
             check_for_redirect(response)
         except requests.HTTPError:
             print(f'Книги с номером {book} не существует')
             continue
+        book_meta = get_book_meta(book)
 
-        with open (f'./books/id{book}.txt', 'wb') as file:
+        with open (f"./books/{book_meta['author']} - {book_meta['title']}.txt", 'wb') as file:
             file.write(response.content)
 
 

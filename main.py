9751fabc36eea56484a8a1cf51dccd812e5b0a76
MSMikl <77015283+MSMikl@users.noreply.github.com>
2022-06-17 18:28:22 +0300

    redirect exceptions handling added

--- main.py
+++ main.py
@@ -4,10 +4,26 @@ import requests
 
 Path('./books').mkdir(parents=True, exist_ok=True)
 
-for book in range(10):
-    url = 'https://tululu.org/txt.php?id='
-    response = requests.get(url=f'{url}{book}')
-    response.raise_for_status()
+def check_for_redirect(content):
+    if content.history:
+        raise requests.HTTPError
+    return
 
-    with open (f'./books/id{book}.txt', 'wb') as file:
-        file.write(response.content)
+def main():
+
+    for book in range(10):
+        url = 'https://tululu.org/txt.php?id='
+        response = requests.get(url=f'{url}{book}')
+        response.raise_for_status()
+        try:
+            check_for_redirect(response)
+        except requests.HTTPError:
+            print(f'Книги с номером {book} не существует')
+            continue
+
+        with open (f'./books/id{book}.txt', 'wb') as file:
+            file.write(response.content)
+
+
+if __name__ == '__main__':
+    main()

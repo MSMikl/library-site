d067ec7c0f0f9accfde0bf10b9e11d467c80ddb8
MSMikl <77015283+MSMikl@users.noreply.github.com>
2022-06-20 20:34:38 +0300

    Update parse_tululu_category.py

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -6,20 +6,21 @@ import requests
 
 from bs4 import BeautifulSoup
 
+
 def main():
     category_index = 55
     url = f"https://tululu.org/l{category_index}"
     response = requests.get(url)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
-    rel_img_url = (
-        soup
-        .find('div', id='content')
-        .find_all('table', class_='d_book')[0]
-        .find('a')['href']
-    )
-    abs_img_url = urljoin(url, rel_img_url)
-    print(abs_img_url)
+    book_urls = [
+        urljoin(url, link.find('a')['href']) for link in (
+            soup
+            .find('div', id='content')
+            .find_all('table', class_='d_book')
+            )
+    ]
+    print(book_urls)
 
 
 if __name__ == '__main__':

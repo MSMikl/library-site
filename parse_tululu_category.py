c4ce1f79d825d4aeac03e865b3d83a98d49289c6
MSMikl <77015283+MSMikl@users.noreply.github.com>
2022-06-20 20:39:43 +0300

    10 pages parced

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -8,18 +8,20 @@ from bs4 import BeautifulSoup
 
 
 def main():
+    book_urls = []
     category_index = 55
-    url = f"https://tululu.org/l{category_index}"
-    response = requests.get(url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
-    book_urls = [
-        urljoin(url, link.find('a')['href']) for link in (
-            soup
-            .find('div', id='content')
-            .find_all('table', class_='d_book')
-            )
-    ]
+    for page_number in range(1, 11):
+        url = f"https://tululu.org/l{category_index}/{page_number}"
+        response = requests.get(url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, 'lxml')
+        book_urls += [
+            urljoin(url, link.find('a')['href']) for link in (
+                soup
+                .find('div', id='content')
+                .find_all('table', class_='d_book')
+                )
+        ]
     print(book_urls)
 
 

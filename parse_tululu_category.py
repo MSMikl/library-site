e944847c54ab5243eadc612c8bdb94a2ab2b9501
MSMikl <77015283+MSMikl@users.noreply.github.com>
2022-06-21 11:43:28 +0300

    new start args added

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -4,6 +4,7 @@ import os
 import sys
 from time import sleep
 
+from pathlib import Path
 from urllib.parse import urljoin
 
 import requests
@@ -29,8 +30,42 @@ def main():
         help='Номер последней страницы для скаичвания',
         default=1
     )
+    parser.add_argument(
+        '-d',
+        '--dest_folder',
+        type=str,
+        help='Папка для скачивания',
+        default='./'
+    )
+    parser.add_argument(
+        '-si',
+        '--skip_imgs',
+        action='store_const',
+        const=True,
+        help='Не скачивать обложки'
+    )
+    parser.add_argument(
+        '-st',
+        '--skip_txt',
+        action='store_const',
+        const=True,
+        help="Не скачивать txt"
+    )
+    parser.add_argument(
+        '-j',
+        '--json_path',
+        type=str,
+        default='./books.json',
+        help='Путь к json-файлу с информацией'
+        
+    )
     args = parser.parse_args()
-    
+
+    Path(os.path.join('.', args.dest_folder)).mkdir(
+        parents=True,
+        exist_ok=True
+    )
+    os.chdir(os.path.join('.', args.dest_folder))
     books_data = []
     book_urls = []
     category_index = 55
@@ -77,7 +112,7 @@ def main():
             continue
         txt_url = urljoin(book_url, book_meta['txt_url'])
         book_filename = f"{(book_meta['title'])}.txt"
-        while True:
+        while not args.skip_txt:
             try:
                 download_content(txt_url, book_filename, 'books/')
             except requests.HTTPError:
@@ -97,8 +132,10 @@ def main():
             break
         if skip_book:
             continue
-        book_meta['book_path'] = os.path.join('books/', book_filename)
-        if book_meta.get('image_url'):
+        if not args.skip_txt:
+            book_meta['book_path'] = os.path.join('books/', book_filename)
+        
+        if book_meta.get('image_url') and not args.skip_imgs:
             image_url = urljoin(book_url, book_meta['image_url'])
             image_name = book_meta['image_url'].split('/')[-1]
             while True:
@@ -123,7 +160,7 @@ def main():
         del book_meta['txt_url']
         books_data.append(book_meta)
 
-    with open('books.json', 'w', encoding='UTF-8') as file:
+    with open(os.path.join('.', args.json_path), 'w', encoding='UTF-8') as file:
         json.dump(books_data, file, ensure_ascii=False, indent=4)
 
 

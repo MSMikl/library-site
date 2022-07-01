531f6c92acaae9b89a3f39153922bd89e0aa9fea
MSMikl <michalbl4@gmail.com>
2022-07-01 08:04:46 +0300

    reload script moved to render_site

--- render_website.py
+++ render_website.py
@@ -1,14 +1,44 @@
+import json
 import os
 import sys
 
-from livereload import Server, shell
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server
+from more_itertools import chunked
+
+
+def rebuild_pages():
+    page_size = 20
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    with open('books.json', 'r', encoding='UTF-8') as file:
+        books = json.load(file)
+    os.makedirs('./pages', exist_ok=True)
+    paged_books = list(chunked(books, page_size))
+    total_pages = len(paged_books)
+    for page_number, page in enumerate(paged_books):
+        chunked_books = list(chunked(page, 2))
+        template = env.get_template('template.html')
+        rendered_page = template.render(
+            books=chunked_books,
+            total_pages=total_pages + 1,
+            current_page=page_number + 1
+        )
+        with open(os.path.join(
+            'pages',
+            f"index{page_number + 1}.html"
+        ), 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 
 
 def main():
     exec_path = sys.executable.replace(os.sep, '/')
     print(exec_path)
     server = Server()
-    server.watch('template.html', shell(f'{exec_path} on_reload.py'))
+    server.watch('template.html', rebuild_pages())
     server.serve(root='.')
 
 
